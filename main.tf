06fde0cef2d8f5a51f8737440d379de6c9714ed2
김재진 <53048187+wowls1227@users.noreply.github.com>
2025-03-31 11:31:29 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,13 @@
 terraform {
+
   required_providers {
-    pgp = {
-      source = "ekristen/pgp"
-      version = "0.2.4"
-    }
     aws = {
-      version = "5.30.0"
+      source = "jjtfe.ddimtech.click/test1/aws"
+      version = "5.93.0"
     }
   }
 }
 
-provider "null" {}
-
-resource "null_resource" "example_sleep" {
-  # 100,000초 동안 기다리기
-  provisioner "local-exec" {
-    command = "sleep 100000"
-  }
+provider "aws" {
+  # Configuration options
 }
